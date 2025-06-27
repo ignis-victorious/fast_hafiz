30d0b08f833b1da66dbbe764a55ae58beac89b31
Emmanuele Levi <ignisvictorious@gmail.com>
2025-06-27 16:51:23 +0100

    working

--- main.py
+++ main.py
@@ -30,12 +30,26 @@ def create_user(user: User) -> dict[str, str | int]:
     return {"message": f"User {user.name} added successfully", "age": user.age}
 
 
+#  Works with: {"Name": "elle", "price": 11.99} or /search/?name=elle&price=11.99
+@app.get(path="/search/")
+#  Works with: {"Name": "elle", "price": 11.99} or /search/?name=elle&price=11.99
+# @app.get(path="/search")
+def search_item(name: str, price: float) -> dict[str, str | float]:
+    return {"Name": name, "price": price}
+
+
+#  Works with: /products/123?show_details=true
+@app.get(path="/products/{product_id}")
+def get_product(product_id: int, show_details: bool = False) -> dict[str, int | bool]:
+    return {"Product Id": product_id, "Show Details": show_details}
+
+
 @app.get(path="/student/{id}")
 def get_id(id: int) -> dict[str, str]:
-    return {"message": f"Student id is: {id}"}
+    return {"message": f"Student's id is: {id}"}
 
 
-@app.post(path="/create-student")
+@app.post(path="/create-student/")
 def create_student(student: Student) -> dict[str, str | int]:
     return {
         "message": "User created successfully",
@@ -45,12 +59,15 @@ def create_student(student: Student) -> dict[str, str | int]:
     }
 
 
-# @app.get(path="/student-details")
-# # @app.get(path="/student-details")
-# def student_details(student: Student) -> tuple[str]:
-#     return (
-#         f"The student name and surname are: {student.fname} {student.lname} while the age is: {student.age}",
-#     )
+#  Works with: /create-student/?id=1000&name=erre&age=21&email=erre@erre.com
+@app.get(path="/student-details")
+# @app.get(path="/student-details/")
+def student_details(student: Student) -> dict[str, int | str]:
+    return {
+        "id": student.id,
+        "name": student.name,
+        "age": student.age,
+    }  # return (f"The student details are: id: {student.id}, name {student.name}, age: {student.age}",)
 
 
 # def main():
